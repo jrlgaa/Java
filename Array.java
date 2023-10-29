aea6f12b814cd37d1f53548247e11d0bcb6d8b99
Jeriel Gaa <jerielgaa142@gmail.com>
2023-10-29 22:40:04 +0800

    Array.java

--- Array.java
+++ Array.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 
-public class Main {
+public class Array {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
               
         String Foods[] = {"Chicken adobo ", "Pork Sinigang ", "Kare-Kare"};
         String Ingredients[] = {"Pineapple Juice ", "Radish ", "Peanuts"};
         String Location[] = {"Batangas ", "Manila ", "Rizal"};
-        
-        int Select;
+
         System.out.println("Select Food \n0: Chicken adobo \n1: Pork sinigang \n2: Kare-kere ");
-        Select = input.nextInt();
+        int Foods0 = input.nextInt();
         System.out.println("Select Ingredients \n0: Pineapple Juice \n1: Radish \n2: Peanuts ");
-        Select = input.nextInt();
+        int Ingredients1 = input.nextInt();
         System.out.println("Select Location \n0: Batangas \n1: Manila \n2: Rizal ");
-        Select = input.nextInt();
+        int Location2 = input.nextInt();
         System.out.println();
-        System.out.println("I have a " + Foods[Select] + " with ingredient of " + Ingredients[Select] + " and made in " + Location[Select] + ".");
+        System.out.println("I have a " + Foods[Foods0] + " with ingredient of " + Ingredients[ingredients1] + " and made in " + Location[Location2] + ".");
     }
 }
