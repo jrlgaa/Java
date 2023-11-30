2093553d03168e8650d7b5fa20eb485e2cf785d7
Jeriel Gaa <jerielgaa142@gmail.com>
2023-11-30 17:30:59 +0800

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,20 +1,76 @@
 import java.util.Scanner;
 
 public class Array {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-              
-        String Foods[] = {"Chicken adobo ", "Pork Sinigang ", "Kare-Kare"};
-        String Ingredients[] = {"Pineapple Juice ", "Radish ", "Peanuts"};
-        String Location[] = {"Batangas ", "Manila ", "Rizal"};
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-        System.out.println("Select Food \n0: Chicken adobo \n1: Pork sinigang \n2: Kare-kere ");
-        int Foods0 = input.nextInt();
-        System.out.println("Select Ingredients \n0: Pineapple Juice \n1: Radish \n2: Peanuts ");
-        int Ingredients1 = input.nextInt();
-        System.out.println("Select Location \n0: Batangas \n1: Manila \n2: Rizal ");
-        int Location2 = input.nextInt();
-        System.out.println();
-        System.out.println("I have a " + Foods[Foods0] + " with ingredient of " + Ingredients[ingredients1] + " and made in " + Location[Location2] + ".");
+    int numbers[][] = {
+      {15, 17, 18, 16},
+      {20, 19, 55, 70},
+      {21, 54, 56, 71},
+      {80, 70, 32, 14}
+    };
+    System.out.println("     Array Table:");
+    System.out.println("|----|----|----|----|");
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.print("|");
+      for (int j = 0; j < 4; j++) {
+        System.out.print(" " + numbers[i][j] + " |");
+      }
+      System.out.println("\n|----|----|----|----|");
     }
+
+    while (true) {
+      System.out.print("\nEnter Cell no.1: ");
+      int cell1 = input.nextInt();
+      if (cell1 > 16) {
+        System.out.println("Please input number from 1-16");
+        continue;
+      }
+      System.out.print("Enter Cell no.2: ");
+      int cell2 = input.nextInt();
+      if (cell2 > 16) {
+        System.out.println("Please input number from 1-16");
+        continue;
+      }
+      System.out.print("Choose an operator (+, -, *, /): ");
+      char operator = input.next().charAt(0);
+
+      int num1 = numbers[(cell1 - 1) / 4][(cell1 - 1) % 4];
+      int num2 = numbers[(cell2 - 1) / 4][(cell2 - 1) % 4];
+      int result = 0;
+
+      switch (operator) {
+        case '+':
+          result = num1 + num2;
+
+          break;
+        case '-':
+          result = num1 - num2;
+          break;
+        case '*':
+          result = num1 * num2;
+          break;
+        case '/':
+          if (num2 != 0) {
+            result = num1 / num2;
+          } else {
+            System.out.println("Cannot divide by zero.");
+            return;
+          }
+          break;
+        default:
+          System.out.println("Invalid operator.");
+      }
+
+      System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+      System.out.print("Do you want to exit (Y/N): ");
+      char Continue = input.next().charAt(0);
+
+      if (Continue != 'N' && Continue != 'n') {
+        System.out.print("Program Exit.");
+        return;
+      }
+    }
+  }
 }
