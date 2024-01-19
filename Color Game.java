24fe3587653aa8cbc1058700766d9eaac7e2375c
Jeriel Gaa <jerielgaa142@gmail.com>
2024-01-19 11:33:09 +0800

    Update Color Game.java

--- Color Game.java
+++ Color Game.java
@@ -1,31 +1,25 @@
 import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		println("\n\t\t    WELCOME TO COLOR GAME");
-		println("\t|-----------|------------|-----------|");
-    // 11 "-"
-		println("\t|    RED    |    WHITE   |    GREEN  |");
-    // 11 "-"
-		println("\t|-----------|------------|-----------|");
-		println("\t|    BLUE   |    YELLOW  |    PINK   |");
-    // 11 "-"
-		println("\t|-----------|------------|-----------|");
+		System.out.println("\n\t\t    WELCOME TO COLOR GAME");
+		System.out.println("\t|-----------|------------|-----------|");
+		System.out.println("\t|    RED    |    WHITE   |    GREEN  |");
+		System.out.println("\t|-----------|------------|-----------|");
+		System.out.println("\t|    BLUE   |    YELLOW  |    PINK   |");
+		System.out.println("\t|-----------|------------|-----------|");
 
 		while (true) {
-
-			print("\n\tChoose Color: ");
+			System.out.print("\n\tChoose Color: ");
 			String chosenColor = input.next().toLowerCase();
 
 			if (isValidColor(chosenColor)) {
-
-				print("\tDo you want to Draw? Y/N: ");
+				System.out.print("\tDo you want to Draw? Y/N: ");
 				char draw = Character.toLowerCase(input.next().charAt(0));
 
 				if (draw != 'y') {
-					println("\tThank You For Playing");
+					System.out.println("\tThank You For Playing");
 					System.exit(0);
 				} else {
 					int cube1 = (int)(Math.random() * 6) + 1;
@@ -35,20 +29,22 @@ public class Main {
 					int cube3 = (int)(Math.random() * 6) + 1;
 					System.out.println("\tCube 3: " + getColorName(cube3));
 
-					if (chosenColor.equals(getColorName(cube1)) || chosenColor.equals(getColorName(cube2)) || chosenColor.equals(getColorName(cube3))) {
-						println("\tYou won");
-					} else if ((chosenColor.equals(getColorName(cube1)) && chosenColor.equals(getColorName(cube2))) ||
-							   (chosenColor.equals(getColorName(cube1)) && chosenColor.equals(getColorName(cube3))) ||
-							   (chosenColor.equals(getColorName(cube2)) && chosenColor.equals(getColorName(cube3)))) {
-						println("\tYou won x2");
-					} else if (chosenColor.equals(getColorName(cube1)) && chosenColor.equals(getColorName(cube2)) && chosenColor.equals(getColorName(cube3))) {
-						println("\tYou won x3");
+					if (chosenColor.equalsIgnoreCase(getColorName(cube1)) ||
+							chosenColor.equalsIgnoreCase(getColorName(cube2)) ||
+							chosenColor.equalsIgnoreCase(getColorName(cube3))) {
+						System.out.println("\tResult: You won");
+					} else if ((chosenColor.equalsIgnoreCase(getColorName(cube1)) && chosenColor.equalsIgnoreCase(getColorName(cube2))) ||
+								(chosenColor.equalsIgnoreCase(getColorName(cube1)) && chosenColor.equalsIgnoreCase(getColorName(cube3))) ||
+				            	(chosenColor.equalsIgnoreCase(getColorName(cube2)) && chosenColor.equalsIgnoreCase(getColorName(cube3)))) {
+					System.out.println("\tResult: You won x2");
+					} else if (chosenColor.equalsIgnoreCase(getColorName(cube1)) && chosenColor.equalsIgnoreCase(getColorName(cube2)) && chosenColor.equalsIgnoreCase(getColorName(cube3))) {
+					System.out.println("\tResult: You won x3");
 					} else {
-						println("\tYou Lose!");
+						System.out.println("\tResult: You Lose!");
 					}
 				}
 			} else {
-				println("\tPlease choose a valid color from the box");
+				System.out.println("\tPlease choose a valid color from the box");
 			}
 		}
 	}
@@ -61,26 +57,19 @@ public class Main {
 	static String getColorName(int number) {
 		switch (number) {
 		case 1:
-			return "red";
+			return "Red";
 		case 2:
-			return "white";
+			return "White";
 		case 3:
-			return "green";
+			return "Green";
 		case 4:
-			return "blue";
+			return "Blue";
 		case 5:
-			return "yellow";
+			return "Yellow";
 		case 6:
-			return "pink";
+			return "Pink";
 		default:
 			return "";
 		}
 	}
-
-	public static void println(String words) {
-		System.out.println(words);
-	}
-
-	public static void print(String words) {
-		System.out.print(words);
-	}
+}
